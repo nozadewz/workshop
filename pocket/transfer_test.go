76891d55dae28b8ad77e68276ad5eb101dcc5dd1
Nattapan Meepiean <nattapan.meepiean@scbtechx.io>
2023-01-15 01:40:23 +0700

    Add more coverage on transfer unit-test

--- pocket/transfer_test.go
+++ pocket/transfer_test.go
@@ -59,7 +59,46 @@ func TestTransfer(t *testing.T) {
 			t.Errorf("there were unfulfilled expections: %s", err)
 		}
 	})
-	t.Run("Transfer but update balance in DB failed return HTTP Internal error", func(t *testing.T) {
+	t.Run("Transfer but update sourcePocket balance in DB failed return HTTP Internal error", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		col := []string{"id", "name", "category", "currency", "balance"}
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(1).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(1, "apocket", "A", "THB", 100.0))
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(2).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(2, "bpocket", "B", "THB", 50.0))
+		mock.ExpectBegin()
+		mock.ExpectExec("UPDATE pockets SET (.+)").WithArgs(50.0, 1).WillReturnError(driver.ErrBadConn)
+		mock.ExpectRollback()
+
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+
+	t.Run("Transfer but update destinationPocket balance in DB failed return HTTP Internal error", func(t *testing.T) {
 		e := echo.New()
 		body := bytes.NewBufferString(`{
 			"source_cloud_pocket_id": 1,
@@ -99,7 +138,7 @@ func TestTransfer(t *testing.T) {
 		}
 	})
 
-	t.Run("Transfer but insert txn history in DB failed return HTTP Internal error", func(t *testing.T) {
+	t.Run("Transfer but insert sourcePocket txn history in DB failed return HTTP Internal error", func(t *testing.T) {
 		e := echo.New()
 		body := bytes.NewBufferString(`{
 			"source_cloud_pocket_id": 1,
@@ -134,6 +173,159 @@ func TestTransfer(t *testing.T) {
 
 		if assert.NoError(t, err) {
 			assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+
+	t.Run("Transfer get sourcePocket failed return HTTP Internal error", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		mock.ExpectPrepare("select (.+) from pockets").WillReturnError(assert.AnError)
+
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+	t.Run("Transfer get destinationPocket failed return HTTP Internal error", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		col := []string{"id", "name", "category", "currency", "balance"}
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(1).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(1, "apocket", "A", "THB", 100.0))
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(2).
+			WillReturnError(assert.AnError)
+
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+	t.Run("Transfer Bind request body failed return HTTP StatusBadRequest", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": "test",
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+	t.Run("Transfer request amount is less than 0 return HTTP StatusBadRequest", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": -50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
+	t.Run("Transfer Insufficient balance return HTTP StatusBadRequest", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		col := []string{"id", "name", "category", "currency", "balance"}
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(1).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(1, "apocket", "A", "THB", 10.0))
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(2).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(2, "apocket", "A", "THB", 10.0))
+		h := handler{db: db}
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusBadRequest, rec.Code)
 		}
 		if err := mock.ExpectationsWereMet(); err != nil {
 			t.Errorf("there were unfulfilled expections: %s", err)
