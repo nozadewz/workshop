78db8e679a95ace112e6acf9b28a678cb30ab329
Nattapan Meepiean <nattapan.meepiean@scbtechx.io>
2023-01-15 09:37:14 +0700

    add unit-test destinationPocket fail

--- pocket/transfer_test.go
+++ pocket/transfer_test.go
@@ -211,6 +211,47 @@ func TestTransfer(t *testing.T) {
 			t.Errorf("there were unfulfilled expections: %s", err)
 		}
 	})
+	t.Run("Transfer but insert destinationPocket txn history in DB failed return HTTP Internal error", func(t *testing.T) {
+		e := echo.New()
+		body := bytes.NewBufferString(`{
+			"source_cloud_pocket_id": 1,
+			"destination_cloud_pocket_id": 2,
+			"amount": 50.00,
+			"description":"Transfer from Travel fund to savings"
+		}`)
+		req := httptest.NewRequest(http.MethodPost, "/cloud-pockets/transfer", body)
+		req.Header.Add(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		db, mock, err := sqlmock.New()
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+		col := []string{"id", "name", "category", "currency", "balance"}
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(1).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(1, "apocket", "A", "THB", 100.0))
+		mock.ExpectPrepare("select (.+) from pockets").
+			ExpectQuery().WithArgs(2).
+			WillReturnRows(sqlmock.NewRows(col).AddRow(2, "bpocket", "B", "THB", 50.0))
+		mock.ExpectBegin()
+		mock.ExpectExec("UPDATE pockets SET (.+)").WithArgs(50.0, 1).WillReturnResult(driver.RowsAffected(1))
+		mock.ExpectExec("UPDATE pockets SET (.+)").WithArgs(100.0, 2).WillReturnResult(driver.RowsAffected(1))
+		mock.ExpectExec("INSERT INTO transaction_history (.+)").WillReturnResult(driver.RowsAffected(1))
+		mock.ExpectExec("INSERT INTO transaction_history (.+)").WillReturnError(assert.AnError)
+		mock.ExpectRollback()
+
+		h := New(config.FeatureFlag{}, db)
+		err = h.Transfer(c)
+
+		if assert.NoError(t, err) {
+			assert.Equal(t, http.StatusInternalServerError, rec.Code)
+		}
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expections: %s", err)
+		}
+	})
 	t.Run("Transfer get destinationPocket failed return HTTP Internal error", func(t *testing.T) {
 		e := echo.New()
 		body := bytes.NewBufferString(`{
